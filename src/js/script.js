6777c35142b5866f1b09e8e02dd969814785994e
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-11-20 23:05:18 +0300

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,142 @@
+angular.module('app', ['btford.socket-io'])
+  .controller('ChatController', function (Chat, Connection) {
+    this.getDialogs = Chat.getDialogs;
+    this.openDialog = Chat.openDialog;
+    this.getCurrentDialog = Chat.getCurrentDialog;
+    this.sendMessage = function () {
+      Chat.sendMessage(this.text);
+      this.text = '';
+    }.bind(this);
+
+    this.login = Connection.login;
+    this.logout = Connection.logout;
+    this.isLoggedIn = Connection.isLoggedIn;
+  })
+  .factory('socket', function ($window, socketFactory) {
+    return socketFactory({
+      prefix: '',
+      ioSocket: $window.io.connect('http://192.168.1.34:8080')
+    });
+  })
+  .factory('Connection', function ($q, socket, Chat) {
+
+    function onMessage (message, callback) {
+      callback && callback();
+      console.log('Received a message');
+      var dialogs = Chat.getDialogs();
+      var activeUserId;
+      dialogs.forEach(function (dialog) {
+        if (dialog.active) {
+          activeUserId = dialog._id;
+        }
+      });
+      console.log(activeUserId);
+      console.log(message);
+      if (activeUserId && (message.from === activeUserId || message.to === activeUserId)) {
+        console.log('Pushing the message');
+        Chat.getCurrentDialog().push(message);
+      }
+    }
+
+    function login (userId) {
+      return $q(function (resolve) {
+        socket.emit('login', {id: userId, regId: 'APA91bHpAf7VKBsCrzOtpeRnWuBim3NUgeSb6jYl4zKYDIux5NRWZQdyv8kFyWJm2JquG7LPPcEDk2p_kaX900WqQ8XH3pzys2zCtr4wCDXXqnH8ee8uGzBgE_B0ZekE0apzW4bDVREBaWWUld5DfZQVtsip6Q1zkcQvJCfKy7wKQEC9b58jKVA'}, function () {
+          console.log('Logged in as', userId);
+          loggedInAs = userId;
+          socket.emit('getDialogs', function (dialogs) {
+            Chat.setDialogs(dialogs);
+            resolve();
+          });
+          socket.on('message', onMessage);
+        });
+      });
+    }
+
+    socket.on('connect', function () {
+      console.log('Connected or reconnected');
+      if (loggedInAs) {
+        socket.removeListener('message', onMessage);
+        Chat.clearDialogs();
+        login(loggedInAs)
+          .then(function () {
+            if (Chat.getCurrentSession()) {
+              Chat.openDialog(
+                Chat.getDialogs().indexOf(
+                  Chat.getDialogs().filter(function (dialog) {
+                    return dialog._id === Chat.getCurrentSession();
+                  })[0]
+                )
+              );
+            }
+          });
+      }
+    });
+
+    var loggedInAs = null;
+
+    return {
+      isLoggedIn: function () {
+        return !!loggedInAs;
+      },
+      login: login,
+      logout: function () {
+        socket.emit('logout', function () {
+          loggedInAs = null;
+          socket.removeListener('message', onMessage);
+          Chat.clearDialogs();
+        });
+      }
+    }
+  })
+  .factory('Chat', function (socket) {
+    var dialogs = [];
+
+    var currentDialog = [];
+
+    var currentSession = null;
+
+    return {
+      getDialogs: function () {
+        return dialogs;
+      },
+      setDialogs: function (newValue) {
+        dialogs = newValue;
+      },
+      openDialog: function (index) {
+        var dialog = dialogs[index];
+        socket.emit('startSession', dialog._id, function (messages) {
+          console.log('Started or restarted session with', dialog._id);
+          currentSession = dialog._id;
+          currentDialog = messages;
+          dialogs.forEach(function (dialog) {
+            dialog.active = false;
+          });
+          dialog.active = true;
+        });
+      },
+      getCurrentSession: function () {
+        return currentSession;
+      },
+      getCurrentDialog: function () {
+        return currentDialog;
+      },
+      clearDialogs: function () {
+        dialogs = [];
+        currentDialog = [];
+      },
+      sendMessage: function (text) {
+        var to;
+        dialogs.forEach(function (dialog) {
+          if (dialog.active) {
+            to = dialog._id;
+          }
+        });
+        if (to) {
+          socket.emit('message', {
+            text: text,
+            to: to
+          });
+        }
+      }
+    };
+  });
